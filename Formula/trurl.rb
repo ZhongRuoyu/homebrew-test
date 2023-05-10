b8c1f2c86a25fdc1eb2113ad2b1b1db0aae2dce2
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-05-10 06:49:54 +0000

    trurl: add 0.6 bottle.

--- Formula/trurl.rb
+++ Formula/trurl.rb
@@ -6,6 +6,13 @@ class Trurl < Formula
   license "curl"
   head "https://github.com/curl/trurl.git", branch: "master"
 
+  bottle do
+    root_url "https://github.com/ZhongRuoyu/homebrew-test/releases/download/trurl-0.6"
+    sha256 cellar: :any_skip_relocation, ventura:  "cfc040c6a05e289e8a9d5c600f2e0671141a652881db1a752a24232fbcca010c"
+    sha256 cellar: :any,                 monterey: "adf9eb8045c343f590a3e5d99ecc96cc1e0ea31335cc4d869d9c39933b5fe60a"
+    sha256 cellar: :any,                 big_sur:  "9c3fc3fd14624b8e0771e776098b5c6848cd8c9bfb15ab3023974b26b42b3e6f"
+  end
+
   uses_from_macos "curl", since: :ventura # uses CURLUE_NO_ZONEID, available since curl 7.81.0
 
   def install
