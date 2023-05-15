c9a97352f7a1b1bf82d8d2dc005cb41c9ae3491b
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-05-15 01:48:06 +0000

    trurl: update 0.6_2 bottle.

--- Formula/trurl.rb
+++ Formula/trurl.rb
@@ -8,11 +8,11 @@ class Trurl < Formula
   head "https://github.com/curl/trurl.git", branch: "master"
 
   bottle do
-    root_url "https://github.com/ZhongRuoyu/homebrew-test/releases/download/trurl-0.6_1"
-    sha256 cellar: :any_skip_relocation, ventura:      "ad01053c7bc538f78e5180c5126eb39e238e02a13b9fe9f75c1a61fe4edbf80a"
-    sha256 cellar: :any,                 monterey:     "cb50308e40d13b44819af8bb507c6eab37ce182c48e79da21a9f7ef0e816f639"
-    sha256 cellar: :any,                 big_sur:      "6303182cf309e674fca693591727c03ff91c2723e10756431f05816688704795"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "af1c401fa649d616caa81f96bcc8b5f8ff72e3daedbcc54f06dafd17916c7151"
+    root_url "https://ghcr.io/v2/zhongruoyu/test-gh-packages"
+    sha256 cellar: :any_skip_relocation, ventura:      "98b18881ba682cff302bfa4932294ce7e1d994a67298f1b5db6b4322241e4f74"
+    sha256 cellar: :any,                 monterey:     "88bcaf5d71bc870a8615062343a40a256c40c0dc46f464f0883ce05db1f7acb4"
+    sha256 cellar: :any,                 big_sur:      "5a89f0838c21393eb11b2047aaf915f1b4a15418bdc16a0807220942aa3b444a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "d599d87b9077d741d954a136079135aa3af42c15262b9964637fe8ef4c887271"
   end
 
   uses_from_macos "curl", since: :ventura # uses CURLUE_NO_ZONEID, available since curl 7.81.0
