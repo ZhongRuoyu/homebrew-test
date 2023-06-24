51372e56b057a76a395a0eb26a38afb3122ef82a
Ruoyu Zhong <zhongruoyu@outlook.com>
2023-06-24 11:52:30 +0800

    trurl 0.8

--- Formula/trurl.rb
+++ Formula/trurl.rb
@@ -1,10 +1,9 @@
 class Trurl < Formula
   desc "Command-line tool for URL parsing and manipulation"
   homepage "https://curl.se/trurl/"
-  url "https://github.com/curl/trurl/archive/refs/tags/trurl-0.6.tar.gz"
-  sha256 "4564dff7441d33a29aa02fe64bea7ef0809d9fabc1609ac5b50ca5503e81caa6"
+  url "https://github.com/curl/trurl/archive/refs/tags/trurl-0.8.tar.gz"
+  sha256 "7baccde1620062cf8c670121125480269b41bdc81bd4015b7aabe33debb022c6"
   license "curl"
-  revision 4
   head "https://github.com/curl/trurl.git", branch: "master"
 
   bottle do
