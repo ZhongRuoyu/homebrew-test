c628485866458650431c10c6a855418ebe6f221c
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-01-31 00:50:35 +0800

    bat 0.24.0

--- Formula/bat.rb
+++ Formula/bat.rb
@@ -1,25 +1,10 @@
 class Bat < Formula
   desc "Clone of cat(1) with syntax highlighting and Git integration"
   homepage "https://github.com/sharkdp/bat"
+  url "https://github.com/sharkdp/bat/archive/refs/tags/v0.24.0.tar.gz"
+  sha256 "907554a9eff239f256ee8fe05a922aad84febe4fe10a499def72a4557e9eedfb"
   license any_of: ["Apache-2.0", "MIT"]
-  revision 4
-
-  # TODO: Remove `stable` block when we can use unversioned `libgit2`.
-  stable do
-    # TODO: check if we can use unversioned `libgit2` at version bump.
-    # Remove `stable` and `head` blocks when this is done.
-    # See comments below for details.
-    url "https://github.com/sharkdp/bat/archive/refs/tags/v0.23.0.tar.gz"
-    sha256 "30b6256bea0143caebd08256e0a605280afbbc5eef7ce692f84621eb232a9b31"
-
-    # To check for `libgit2` version:
-    # 1. Search for `libgit2-sys` version at https://github.com/sharkdp/bat/blob/v#{version}/Cargo.lock
-    # 2. If the version suffix of `libgit2-sys` is newer than +1.5.*, then:
-    #    - Use the corresponding `libgit2` formula.
-    #    - Remove `LIBGIT2_SYS_USE_PKG_CONFIG` env var below.
-    #      See: https://github.com/rust-lang/git2-rs/commit/59a81cac9ada22b5ea6ca2841f5bd1229f1dd659.
-    depends_on "libgit2@1.5"
-  end
+  head "https://github.com/sharkdp/bat.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/zhongruoyu/zhongruoyu-homebrew-test"
@@ -29,19 +14,13 @@ class Bat < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "02424b7900ebd5e1ce4399103086b8024f6abc94218f28298e909d693b5c9303"
   end
 
-  # TODO: Remove `head` block when `stable` uses unversioned `libgit2`.
-  head do
-    url "https://github.com/sharkdp/bat.git", branch: "master"
-    depends_on "libgit2"
-  end
-
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
+  depends_on "libgit2"
   depends_on "oniguruma"
 
   def install
     ENV["LIBGIT2_NO_VENDOR"] = "1"
-    ENV["LIBGIT2_SYS_USE_PKG_CONFIG"] = "1"
     ENV["RUSTONIG_DYNAMIC_LIBONIG"] = "1"
     ENV["RUSTONIG_SYSTEM_LIBONIG"] = "1"
 
@@ -69,7 +48,7 @@ class Bat < Formula
     assert_match "Homebrew test", output
 
     [
-      Formula["libgit2@1.5"].opt_lib/shared_library("libgit2"),
+      Formula["libgit2"].opt_lib/shared_library("libgit2"),
       Formula["oniguruma"].opt_lib/shared_library("libonig"),
     ].each do |library|
       assert check_binary_linkage(bin/"bat", library),
